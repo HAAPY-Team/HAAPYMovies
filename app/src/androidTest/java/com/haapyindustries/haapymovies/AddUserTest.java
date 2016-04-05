c405f4eb290fb7ed003a69629a4c711e389017ad
brohand <superryuk@gmail.com>
2016-04-05 01:07:41 -0400

    Added JUnits.
    Ghost ride the whip.

---
+++ app/src/androidTest/java/com/haapyindustries/haapymovies/AddUserTest.java
@@ -0,0 +1,72 @@
+package com.haapyindustries.haapymovies.models;
+
+
+import android.app.Instrumentation;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
+
+import com.haapyindustries.haapymovies.enums.UserStatus;
+import com.haapyindustries.haapymovies.enums.UserType;
+import com.haapyindustries.haapymovies.models.User;
+import com.haapyindustries.haapymovies.models.UserManager;
+import com.haapyindustries.haapymovies.providers.Database;
+import com.haapyindustries.haapymovies.providers.TestDatabase;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+
+import android.os.Parcel;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
+import org.junit.runner.RunWith;
+
+import java.lang.Exception;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+/**
+ * Created by Aaron on 4/4/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class AddUserTest {
+    protected Context context;
+    protected TestDatabase db;
+
+
+    @Before
+    public void setupTests() {
+        context = InstrumentationRegistry.getTargetContext();
+        db = new TestDatabase(context);
+
+    }
+    public void testAddedUserIsInDatabase() throws Exception {
+        UserManager.addUser("aaron", "pass", "cs", db);
+        assertEquals("aaron", db.getUserFromUsername("aaron").getUsername());
+        assertEquals("aaron", db.getUserFromUsername("aaron").getPassword());
+        assertEquals("aaron", db.getUserFromUsername("aaron").getMajor());
+
+
+    }
+    public void testAddedAdminIsInDatabase() throws Exception {
+        UserManager.addAdmin("patrick", "pass", "cs", db);
+        assertEquals("aaron", db.getUserFromUsername("patrick").getUsername());
+        assertEquals("aaron", db.getUserFromUsername("patrick").getPassword());
+        assertEquals("aaron", db.getUserFromUsername("patrick").getMajor());
+
+        assertEquals(UserType.ADMIN.toString(), db.getUserFromUsername("patrick").getUserType().toString())
+
+
+    }
+
+
+
+}
